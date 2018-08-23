33c8f709c1d000838c773d308e8daa3a76fbb078
aaron.medina <aaron.medina@zapproved.com>
2018-08-23 15:02:35 -0700

    Added linting rules

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const logSomething = options => ({
+    ...options,
+    anotherOption: 'Hello!'
+});
+
+const options = logSomething({one: '1', two: '2'});
+
+console.log(options);
